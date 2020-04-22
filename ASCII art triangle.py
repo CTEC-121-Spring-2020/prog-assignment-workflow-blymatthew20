07a5767a28d9a1c61e82eda22e30f87775f5c1d9
Matthew Bly <skyguard14@iCloud.com>
2020-04-22 12:53:03 -0700

    code for file 2 tested and is working

---
+++ ASCII art triangle.py
@@ -0,0 +1,20 @@
+"""
+CTEC 121
+Matthew Bly
+Program Assignment Workflow
+Program Assigment Workflow Code for file 2
+"""
+
+""" IPO template
+Input(s): list/description
+Process: description of what function does
+Output: return value and description
+"""
+
+def main():
+    print()
+    print(" /\ ")
+    print("/__\ ")
+    print()
+
+main()  
