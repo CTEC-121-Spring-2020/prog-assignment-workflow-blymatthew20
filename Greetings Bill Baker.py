2a690bdb041844569a967370da7a77f6dbb30964
Matthew Bly <skyguard14@iCloud.com>
2020-04-22 12:12:59 -0700

    Completed code for first file and tested

---
+++ Greetings Bill Baker.py
@@ -0,0 +1,21 @@
+"""
+CTEC 121
+Matthew Bly
+Program Assignment Workflow
+Program Assigment Workflow Code for file 1
+"""
+
+""" IPO template
+Input(s): list/description
+Process: description of what function does
+Output: return value and description
+"""
+
+def main():
+    print()
+    print("Hello!")
+    print("Bill Baker")
+    print("How are you doing today?")
+    print()
+
+main()    
